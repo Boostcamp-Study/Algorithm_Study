a5b7057b9283a4392fdda9af82b14b20b37f61cb
jiwan <wldhks1120@naver.com>
2024-12-03 23:44:57 +0900

    박지완 3주차 1, 2, 3 더하기

--- week3/9095_batwan01.py
+++ week3/9095_batwan01.py
@@ -10,13 +10,10 @@
 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
 '''
 
-dp = [i for i in range(12)]
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
+dp = [0, 1, 2, 4]
 
 for i in range(4, 12):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    dp.append(dp[i-1] + dp[i-2] + dp[i-3])
 
 N = int(input())
 
