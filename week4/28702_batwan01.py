eae6904a95782b1ef8b9042dfae7ee6d78a6d8dc
박지완 <wldhks1120@naver.com>
2024-12-12 17:20:13 +0900

    4주차 박지완 FizzBuzz

--- week4/28702_batwan01.py
+++ week4/28702_batwan01.py
@@ -5,3 +5,22 @@
  $i$가 $3$의 배수도 아니고 $5$의 배수도 아닌 경우 $i$를 그대로 출력합니다.
 '''
 
+arr = []
+count = 3
+for _ in range(3):
+    arr.append(input())
+
+for num in arr:
+    if num.isdigit():
+        num_result = int(num) + count
+        if num_result % 5 == 0 and num_result % 3 == 0:
+            print("FizzBuzz")
+        elif num_result % 3 == 0:
+            print("Fizz")
+        elif num_result % 5 == 0:
+            print("Buzz")
+        else:
+            print(num_result)
+        break
+    else:
+        count-=1
