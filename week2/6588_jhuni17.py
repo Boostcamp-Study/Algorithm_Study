571f0f4892e73cd153cbf02c2469bf67f38c7b63
jhuni17 <cj9681197@gmail.com>
2024-12-17 23:30:25 +0900

    12.17 jhuni17 2주차 풀이: 골든바흐의 추측

--- week2/6588_jhuni17.py
+++ week2/6588_jhuni17.py
@@ -1,20 +1,25 @@
 #6588 골드바흐의 추측
+import sys
 
-def is_odd_prime(n):
-    for i in range(3, int(n**0.5)+1, 2):
-        if n % i ==0:
-            return 0
-    return 1
+def find_prime_list(n):
+    is_prime = [True] * (n + 1)
+    for i in range(2, int(n**0.5) + 1):
+        for j in range(i * i, n + 1, i):
+            is_prime[j] = False
+    return is_prime 
+        
+odd_prime_list = find_prime_list(1000000)
 
 def solution(N):
     for i in range(3, int(N/2)+1, 2):
-        if is_odd_prime(i) == 1 and is_odd_prime(N-i):
-            return print(f"{N} = {i} + {N-i}")
+        if odd_prime_list[i] and odd_prime_list[N-i]:
+            print(f"{N} = {i} + {N-i}")
+            return
             
     print("Goldbach's conjecture is wrong.")
 
 while True:
-    N = int(input())
+    N = int(sys.stdin.readline())
     if N == 0:
         break
     solution(N)
